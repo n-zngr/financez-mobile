038636fe38a1d14a8f0c062a474fb5b6e6ccd838
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-02-20 20:22:24 +0100

    refactor: update tab indents

--- hooks/useColorScheme.web.ts
+++ hooks/useColorScheme.web.ts
@@ -2,20 +2,20 @@ import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
 
 /**
- * To support static rendering, this value needs to be re-calculated on the client side for web
- */
+* To support static rendering, this value needs to be re-calculated on the client side for web
+*/
 export function useColorScheme() {
-  const [hasHydrated, setHasHydrated] = useState(false);
+    const [hasHydrated, setHasHydrated] = useState(false);
 
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
+    useEffect(() => {
+        setHasHydrated(true);
+    }, []);
 
-  const colorScheme = useRNColorScheme();
+    const colorScheme = useRNColorScheme();
 
-  if (hasHydrated) {
-    return colorScheme;
-  }
+    if (hasHydrated) {
+        return colorScheme;
+    }
 
-  return 'light';
+    return 'light';
 }
