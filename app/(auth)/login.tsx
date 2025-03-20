4ab919a21f85c8aa24f1731d5aca634fe02767a1
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-20 22:22:43 +0100

    feat(login): implement login page
    - implement login page, connecting to I, manually to be set in .env as of right now
    - login proceeds to (tabs) page after userToken was set

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+import styles from '@/style/style';
+
+const DEV_IP = process.env.DEV_IP;
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch(`http://${DEV_IP}:3000/api/users/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+    
+            const data = await response.json();
+            console.log(data);
+    
+            if (response.ok) {
+                await AsyncStorage.setItem('userToken', data.userId);
+                router.replace('/(tabs)');
+            } else {
+                Alert.alert('Login Failed', data.error);
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+            Alert.alert('Login Error', 'An error occurred during login.');
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => router.push('/(auth)/signup')}>
+        <Text style={styles.buttonText}>Signup</Text>
+      </TouchableOpacity>
+    </View>
+    )
+}
+
+export default Login;
