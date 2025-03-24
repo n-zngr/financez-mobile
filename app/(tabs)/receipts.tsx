a36b1e33ada84fa5592bc8a05a63245744199017
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-24 09:10:40 +0100

    feat(receipts): remove two, replacing with receipts
    - replace two with temporary receipt page

---
+++ app/(tabs)/receipts.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabTwoScreen() {
+    return (
+        <div></div>
+    )
+
+}
